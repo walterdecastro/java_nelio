a6281c08da2cbfd109e52f595940f8f26c7feb5d
walterdecastro <walterdc1995@gmail.com>
2022-10-28 18:29:49 -0300

    aula 102 matrizes

--- aula102_matrizes_exercicio_resolvido/src/application/Program.java
+++ aula102_matrizes_exercicio_resolvido/src/application/Program.java
@@ -11,21 +11,21 @@ public class Program {
 		int n = sc.nextInt();
 		int[][] mat = new int[n][n];
 		
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<n; j++) {
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat[i].length; j++) {
 				mat[i][j] = sc.nextInt();
 			}
 		}
 		
 		System.out.println("Main diagonal: ");
-		for (int i=0; i<n; i++) {
+		for (int i=0; i<mat.length; i++) {
 			System.out.print(mat[i][i] + " ");
 		}
 		System.out.println();
 		
 		int count = 0;
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<n; j++) {
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat[i].length; j++) {
 				if (mat[i][j] < 0) {
 					count++;
 				}
