f20b56553e50cb20d32b1e53b84cd0fa4d141744
walterdecastro <walterdc1995@gmail.com>
2022-10-28 18:22:45 -0300

    aula 102 matrizes

---
+++ aula99_exercicio_proposto/src/entities/Employee.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Employee {
+	
+	private Integer id;
+	private String name;
+	private Double salary;
+	
+	public Employee(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	public void increaseSalary(double percentage) {
+		salary += salary * percentage / 100.0;
+	}
+	
+	public String toString() {
+		return id + ", " + name + ", " + String.format("%.2f", salary);
+	}
+	
+	
+	
+	
+}
